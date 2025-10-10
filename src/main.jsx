47faec196528953484861775cd50c48608d3f18e
linhnd <linhnd@d-soft.com.vn>
2025-10-10 08:14:44 +0700

    feat/page 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
-import {
-  BrowserRouter as Routers,
-  Route,
-  Routes as Router,
-} from 'react-router-dom';
+
 import Home from './pages/home.jsx';
 import Blog from './components/blog/Blog.jsx';
 import BlogDetail from './components/blog/BlogDetail.jsx';
@@ -15,23 +12,89 @@ import MyProducts from './components/product/MyProducts.jsx';
 import AddProduct from './components/product/AddProduct.jsx';
 import EditMyProduct from './components/product/EditMyProduct.jsx';
 import DetailProduct from './components/product/DetailProduct.jsx';
+import NotFound from './components/404/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Routers>
-      <App>
-        <Router>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/blog/detail/:id" element={<BlogDetail />} />
-          <Route path="/login" element={<Index />} />
-          <Route path="/account/member" element={<UpdateMember />} />
-          <Route path="/account/product" element={<MyProducts />} />
-          <Route path="/account/product/add" element={<AddProduct />} />
-          <Route path="/account/product/edit/:id" element={<EditMyProduct />} />
-          <Route path="/product/detail/:id" element={<DetailProduct />} />
-        </Router>
-      </App>
-    </Routers>
+    <Router>
+      <Routes>
+        {/* Các route có layout */}
+        <Route
+          path="/"
+          element={
+            <App>
+              <Home />
+            </App>
+          }
+        />
+        <Route
+          path="/blog"
+          element={
+            <App>
+              <Blog />
+            </App>
+          }
+        />
+        <Route
+          path="/blog/detail/:id"
+          element={
+            <App>
+              <BlogDetail />
+            </App>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <App>
+              <Index />
+            </App>
+          }
+        />
+        <Route
+          path="/account/member"
+          element={
+            <App>
+              <UpdateMember />
+            </App>
+          }
+        />
+        <Route
+          path="/account/product"
+          element={
+            <App>
+              <MyProducts />
+            </App>
+          }
+        />
+        <Route
+          path="/account/product/add"
+          element={
+            <App>
+              <AddProduct />
+            </App>
+          }
+        />
+        <Route
+          path="/account/product/edit/:id"
+          element={
+            <App>
+              <EditMyProduct />
+            </App>
+          }
+        />
+        <Route
+          path="/product/detail/:id"
+          element={
+            <App>
+              <DetailProduct />
+            </App>
+          }
+        />
+
+        {/* Trang 404 nằm ngoài App => không có header/footer/menu */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
